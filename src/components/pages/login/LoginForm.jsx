08afeb6a363d3721198770ae7bde0e6b11853212
Romain <romain.grandais@epitech.eu>
2023-10-13 11:32:53 +0200

    🚚 F01 : Rename Login > varible name Handdle to Handle

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,29 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const LoginForm = () => { 
-    
-    const [prenom, setPrenom] = useState("")
+const LoginForm = () => {
+  const [prenom, setPrenom] = useState("");
 
-    const HanddleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        alert(`Bonjour ${prenom}`)
-        setPrenom("") 
-    }
+    alert(`Bonjour ${prenom}`);
+    setPrenom("");
+  };
 
-    const HanddleChange = (event) => {
-        setPrenom(event.target.value)
-    }
+  const handleChange = (event) => {
+    setPrenom(event.target.value);
+  };
 
-
-    return (
-        <div>
-            <form type="submit" onSubmit={HanddleSubmit}>
-                <input type="text"  value={prenom} placeholder='Entrez votre prénom...' onChange={HanddleChange} required/>
-                <button type='submit'   >Accéder à votre espace</button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <form type="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={prenom}
+          placeholder="Entrez votre prénom..."
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Accéder à votre espace</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
