6ee89ad356ada7e2c424dab0398f8796f8ceac2a
Romain <romain.grandais@epitech.eu>
2023-10-16 12:14:39 +0200

    💄 LOGIN: Add style in login page

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { theme } from "../../../theme";
+import styled from "styled-components";
+import { BsPersonCircle } from "react-icons/bs";
+import { IoChevronForward } from "react-icons/io5";
 
 const LoginForm = () => {
   const [prenom, setPrenom] = useState("");
@@ -16,19 +20,107 @@ const LoginForm = () => {
   };
 
   return (
-    <div>
-      <form type="submit" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={prenom}
-          placeholder="Entrez votre prénom..."
-          onChange={handleChange}
-          required
-        />
-        <button type="submit">Accéder à votre espace</button>
-      </form>
-    </div>
+    <LoginFormStyled>
+      <div className="content">
+        <h1>bienvenue chez nous !</h1>
+        <br />
+        <div className="bar"></div>
+        <br />
+        <h2>Connectez-vous</h2>
+        <form type="submit" onSubmit={handleSubmit}>
+          <div className="input-with-icon">
+            <BsPersonCircle className="icon" />
+            <input
+              type="text"
+              value={prenom}
+              placeholder="Entrez votre prénom"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="button-with-icon">
+            <button type="submit">
+              <span>Accéder à mon espace</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </LoginFormStyled>
   );
 };
 
 export default LoginForm;
+
+const LoginFormStyled = styled.div`
+  position: relative;
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+  h1,
+  h2 {
+    color: ${theme.colors.background_white};
+  }
+
+  h1 {
+    font-size: ${theme.fonts.P4};
+  }
+  h2 {
+    font-size: ${theme.fonts.P3};
+  }
+
+  .bar {
+    width: 400px;
+    border: 1px solid ${theme.colors.primary};
+  }
+
+  form {
+    gap: 12px;
+    margin-top: 12px;
+  }
+
+  .input-with-icon {
+    background-color: ${theme.colors.background_white};
+    border-radius: ${theme.borderRadius.round};
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    gap: 12px;
+
+    input {
+      border: none;
+      background-color: ${theme.colors.background_white};
+    }
+
+    .icon {
+      font-size: ${theme.fonts.P0};
+      margin-right: 8px;
+      color: ${theme.colors.dark};
+    }
+    &::placeholder {
+      background: ${theme.colors.background_white};
+      color: ${theme.colors.greyLight};
+    }
+  }
+
+  .button-with-icon {
+    display: flex;
+    justify-content: center;
+    button {
+      padding: 18px 24px;
+      gap: 12px;
+      color: ${theme.colors.background_white};
+      background-color: ${theme.colors.primary};
+      font-weight: ${theme.weights.bold};
+      border-radius: ${theme.borderRadius.round};
+      width: 400px;
+      height: 53px;
+      border: 1px solid ${theme.colors.primary};
+    }
+    button:active {
+      color: ${theme.colors.primary};
+      background-color: ${theme.colors.background_white};
+    }
+  }
+`;
