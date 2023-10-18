e2d38d542cf9e93b232ff6777fdbc332c971dcfa
Romain <romain.grandais@epitech.eu>
2023-10-18 09:51:33 +0200

    ♻️ F04: OrderPage > NavBar > Create NavBarRightSide component

--- src/components/pages/Order/NavBar.jsx
+++ src/components/pages/Order/NavBar.jsx
@@ -1,36 +1,19 @@
 import React from "react";
-import { BsPersonCircle } from "react-icons/bs";
 import Logo from "../../reusable-ui/Logo";
-import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import NavBarRightSide from "./NavBarRightSide";
 
 export default function NavBar({ username }) {
-  const navigate = useNavigate();
-
-  const handleClickHome = (event) => {
-    event.preventDefault();
-    navigate("/");
-  };
-
   return (
     <NavBarStyled>
-      <a href="">
+      <div
+        onClick={() => window.location.reload(false)}
+        style={{ cursor: "pointer" }}
+      >
         <Logo />
-      </a>
-      <div className="navbar-content">
-        <div className="child-left">
-          <h2>
-            Hey, <span>{username}</span>
-          </h2>
-          <a href="" onClick={handleClickHome}>
-            Se déconnecter
-          </a>
-        </div>
-        <div className="icon">
-          <BsPersonCircle />
-        </div>
       </div>
+      <NavBarRightSide username={username} />
     </NavBarStyled>
   );
 }
@@ -48,50 +31,4 @@ const NavBarStyled = styled.nav`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-
-  h2 {
-    display: inline;
-    color: ${theme.colors.greyBlue};
-    font-family: "Open Sans", sans-serif;
-    font-weight: ${theme.weights.regular};
-    font-size: ${theme.fonts.P0};
-    text-align: right;
-  }
-
-  span {
-    color: ${theme.colors.primary};
-    font-weight: ${theme.weights.bold};
-  }
-
-  .navbar-content {
-    display: flex;
-    flex-direction: row;
-    gap: ${theme.spacing.sm};
-  }
-
-  .child-left {
-    display: flex;
-    flex-direction: column;
-  }
-
-  a {
-    background: transparent;
-    border: none;
-    text-align: right;
-    font-size: ${theme.fonts.XXS};
-    color: ${theme.colors.greyBlue};
-    border: none;
-    text-decoration: none;
-    font-weight: ${theme.weights.regular};
-    padding-bottom: 1px;
-  }
-
-  .child-left a:hover {
-    border-bottom: 1px solid ${theme.colors.greyBlue};
-  }
-
-  .icon {
-    color: ${theme.colors.greyBlue};
-    font-size: ${theme.fonts.P4};
-  }
 `;
