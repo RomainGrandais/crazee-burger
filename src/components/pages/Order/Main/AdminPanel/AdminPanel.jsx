e8e9e66fc8fd57a6e318ff677140496cfb7fa6b0
Romain <romain.grandais@epitech.eu>
2023-10-21 16:58:58 +0200

    ♻️F07: OrderPage > Main > AdminPanel > refacto, create AdminTab

--- src/components/pages/Order/Main/AdminPanel/AdminPanel.jsx
+++ src/components/pages/Order/Main/AdminPanel/AdminPanel.jsx
@@ -1,63 +1,15 @@
 import React, { useContext, useState } from "react";
 import styled from "styled-components";
-import { theme } from "../../../../../theme";
-import AdminButton from "./AdminButton";
-import { FiChevronDown, FiChevronUp } from "react-icons/fi";
-import { MdModeEditOutline } from "react-icons/md";
-import { AiOutlinePlus } from "react-icons/ai";
 import AdminContext from "../../../../../context/AdminContext";
 import AdminContent from "./AdminContent";
+import AdminTab from "./AdminTab";
 
 export default function AdminPannel() {
   const infos = useContext(AdminContext);
 
-  const handleClickDown = () => {
-    infos.setIsDown(!infos.isDown);
-  };
-
-  const handleClickAddProduct = () => {
-    infos.setAddProduct(true);
-    infos.setModifProduct(false);
-  };
-
-  const handleClickModifProduct = () => {
-    infos.setAddProduct(false);
-    infos.setModifProduct(true);
-  };
-
   return (
     <AdminPannelStyled>
-      <div className="nav-admin">
-        <div className="up" onClick={handleClickDown}>
-          {infos.isDown ? (
-            <AdminButton
-              icons={<FiChevronUp />}
-              label={""}
-              isSelected={infos.isDown}
-            />
-          ) : (
-            <AdminButton
-              icons={<FiChevronDown />}
-              label={""}
-              isSelected={infos.isDown}
-            />
-          )}
-        </div>
-        <div className="addProcudct" onClick={handleClickAddProduct}>
-          <AdminButton
-            icons={<AiOutlinePlus />}
-            label={"Ajouter un produit"}
-            isSelected={infos.isAddProduct}
-          />
-        </div>
-        <div className="modifProduct" onClick={handleClickModifProduct}>
-          <AdminButton
-            icons={<MdModeEditOutline />}
-            label={"Modifier un produit"}
-            isSelected={infos.isModifProduct}
-          />
-        </div>
-      </div>
+      <AdminTab />
       {!infos.isDown && <AdminContent AddProduct={infos.isAddProduct} />}
     </AdminPannelStyled>
   );
@@ -71,11 +23,4 @@ const AdminPannelStyled = styled.div`
   left: 0;
   z-index: 0;
   width: 100%;
-
-  .nav-admin {
-    height: 44px;
-    padding-left: 70px;
-    display: flex;
-    flex-direction: row;
-  }
 `;
---
+++ src/components/pages/Order/Main/AdminPanel/AdminTab.jsx
@@ -0,0 +1,67 @@
+import React, { useContext, useState } from "react";
+import styled from "styled-components";
+import AdminButton from "./AdminButton";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { MdModeEditOutline } from "react-icons/md";
+import { AiOutlinePlus } from "react-icons/ai";
+import AdminContext from "../../../../../context/AdminContext";
+
+export default function AdminTab() {
+  const infos = useContext(AdminContext);
+
+  const handleClickDown = () => {
+    infos.setIsDown(!infos.isDown);
+  };
+
+  const handleClickAddProduct = () => {
+    infos.setAddProduct(true);
+    infos.setModifProduct(false);
+    infos.setIsDown(false);
+  };
+
+  const handleClickModifProduct = () => {
+    infos.setAddProduct(false);
+    infos.setModifProduct(true);
+    infos.setIsDown(false);
+  };
+  return (
+    <AdminTabStyled>
+      <div className="up" onClick={handleClickDown}>
+        {infos.isDown ? (
+          <AdminButton
+            icons={<FiChevronUp />}
+            label={""}
+            isSelected={infos.isDown}
+          />
+        ) : (
+          <AdminButton
+            icons={<FiChevronDown />}
+            label={""}
+            isSelected={infos.isDown}
+          />
+        )}
+      </div>
+      <div className="addProcudct" onClick={handleClickAddProduct}>
+        <AdminButton
+          icons={<AiOutlinePlus />}
+          label={"Ajouter un produit"}
+          isSelected={infos.isAddProduct}
+        />
+      </div>
+      <div className="modifProduct" onClick={handleClickModifProduct}>
+        <AdminButton
+          icons={<MdModeEditOutline />}
+          label={"Modifier un produit"}
+          isSelected={infos.isModifProduct}
+        />
+      </div>
+    </AdminTabStyled>
+  );
+}
+
+const AdminTabStyled = styled.div`
+  height: 44px;
+  padding-left: 70px;
+  display: flex;
+  flex-direction: row;
+`;
